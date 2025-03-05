1f977ddb255b3d7ea7ccfcfa0f824845bd49ec98
Jaden Dixon-Schulz <jdixonschulz@gmail.com>
2025-03-05 15:04:58 -0500

    planetary goodness

---
+++ src/body.ts
@@ -0,0 +1,62 @@
+import p5, { Vector } from "p5";
+import { p } from "./main";
+
+export class Body {
+  static G: number = 1;
+  position: Vector;
+  velocity: Vector;
+  acceleration: Vector;
+
+  radius: number;
+  mass: number;
+  canMove: boolean;
+
+  constructor(position: [number, number], mass: number, canMove?: boolean) {
+    this.position = new p5.Vector(...position);
+    this.velocity = new p5.Vector();
+    this.acceleration = new p5.Vector();
+
+    this.radius = p.sqrt(mass) * 2;
+    this.mass = mass;
+    this.canMove = canMove ?? true;
+  }
+
+  applyForce(force: Vector) {
+    const applied = p5.Vector.div(force, this.mass) as any as Vector;
+    this.acceleration.add(applied);
+  }
+
+  step() {
+    if (this.canMove) {
+      this.velocity.add(this.acceleration);
+      this.position.add(this.velocity);
+      this.acceleration.set(0, 0);
+    }
+  }
+
+  render() {
+    p.push();
+    p.stroke(0);
+    p.strokeWeight(2);
+    p.fill(200);
+    p.circle(this.position.x, this.position.y, this.radius * 2);
+    p.pop();
+  }
+
+  getCumulativeAttraction(bodies: Body[]): Vector {
+    if (!this.canMove) return new p5.Vector();
+
+    const accumulator = new p5.Vector();
+    for (const body of bodies) {
+      if (body === this) continue;
+      const force = p5.Vector.sub(body.position, this.position).normalize();
+      const distance = p.constrain(force.mag(), 5, 25);
+      const strength = (Body.G * (this.mass * body.mass)) / distance ** 2;
+
+      force.setMag(strength);
+
+      accumulator.add(force);
+    }
+    return accumulator;
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,74 @@
-import p5 from "p5";
+import p5, { Vector } from "p5";
 import "./main.css";
+import { Body } from "./body";
 
 const CANVAS_DIMENSIONS: [number, number] = [1024, 512];
-const CANVAS_COLOR: [number, number, number] = [222, 222, 255];
+const CANVAS_COLOR: [number, number, number, number] = [255, 255, 255, 255];
 function sketch(p: p5) {
+  const bodies: Body[] = [];
   p.setup = () => {
     p.createCanvas(...CANVAS_DIMENSIONS);
     p.background(...CANVAS_COLOR);
+
+    // for (let i = 0; i < 3; i++) {
+    //   const MAX_DIST = 200;
+    //   const x = p.map(
+    //     p.noise(i),
+    //     0,
+    //     1,
+    //     p.width / 2 - MAX_DIST,
+    //     p.width / 2 + MAX_DIST
+    //   );
+    //   const y = p.map(
+    //     p.noise(i + 10000),
+    //     0,
+    //     1,
+    //     p.height / 2 - MAX_DIST,
+    //     p.height / 2 + MAX_DIST
+    //   );
+
+    //   const body = new Body([x, y], p.random(5, 10));
+    //   bodies.push(body);
+    // }
+
+    const DIST_A = 100;
+    const DIST_B = 150;
+
+    bodies.push(new Body([p.width / 2 - DIST_A, p.height / 2 - DIST_A], 8));
+    bodies.push(new Body([p.width / 2 + DIST_A, p.height / 2 + DIST_A], 8));
+    // bodies.push(new Body([p.width / 2 - DIST_A, p.height / 2 + DIST_A], 125));
+    // bodies.push(new Body([p.width / 2 + DIST_A, p.height / 2 - DIST_A], 125));
+
+    bodies[0].velocity.set(20, 0);
+    bodies[1].velocity.set(-20, 0);
+    // bodies[2].velocity.set(0, -20);
+    // bodies[3].velocity.set(0, 20);
+
+    // bodies.push(new Body([p.width / 2 - DIST_B, p.height / 2 - DIST_B], 5));
+    // bodies.push(new Body([p.width / 2 + DIST_B, p.height / 2 + DIST_B], 5));
+    // bodies.push(new Body([p.width / 2 - DIST_B, p.height / 2 + DIST_B], 5));
+    // bodies.push(new Body([p.width / 2 + DIST_B, p.height / 2 - DIST_B], 5));
+
+    // bodies[4].velocity.set(5, 0);
+    // bodies[5].velocity.set(-5, 0);
+    // bodies[6].velocity.set(0, -5);
+    // bodies[7].velocity.set(0, 5);
+
+    // bodies.push(new Body([p.width / 2, p.height / 2], 20));
   };
 
   p.draw = () => {
-    p.background(240);
+    p.background(...CANVAS_COLOR);
+    const forces: Vector[] = [];
+    for (const body of bodies) {
+      forces.push(body.getCumulativeAttraction(bodies));
+    }
+
+    for (let i = 0; i < bodies.length; i++) {
+      bodies[i].applyForce(forces[i]);
+      bodies[i].step();
+      bodies[i].render();
+    }
   };
 }
 
